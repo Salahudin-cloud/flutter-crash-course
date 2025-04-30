96f813501fafd289eaf4bba10d5d90de549db8d6
Salahudin-cloud <ayubi54321@gmail.com>
2025-04-30 20:52:01 +0700

    learn column

--- lib/home.dart
+++ lib/home.dart
@@ -6,22 +6,41 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: const Text(
-            "My Coffee Card",
-            style: TextStyle(color: Colors.white),
-          ),
-          backgroundColor: Colors.brown[700]),
-      body: Container(
-        color: Colors.purple[400],
-        padding: const EdgeInsets.all(15),
-        margin: const EdgeInsets.fromLTRB(15, 15, 0, 0),
-        child: const Text(
-          "Hello Wolrd",
-          style: TextStyle(
-              color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),
-        ),
-      ),
-    );
+        appBar: AppBar(
+            title: const Text(
+              "My Coffee Card",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            backgroundColor: Colors.brown[700]),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              width: 200,
+              color: Colors.brown[500],
+              padding: const EdgeInsets.all(15),
+              child: const Text(
+                "How i like my coffe",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              width: 150,
+              color: Colors.brown[400],
+              padding: const EdgeInsets.all(15),
+              child: const Text(
+                "My Coffe prefs",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,53 @@ import 'package:coffee_card/home.dart';
 void main() {
   runApp(const MaterialApp(home: Home()));
 }
+
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Sanbox",
+              style: TextStyle(fontWeight: FontWeight.bold)),
+          backgroundColor: Colors.grey,
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              color: Colors.grey[500],
+              width: 100,
+              padding: const EdgeInsets.all(10),
+              child: const Text(
+                "Box 1",
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              color: Colors.grey[400],
+              width: 150,
+              padding: const EdgeInsets.all(10),
+              child: const Text(
+                "Box 2",
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              color: Colors.grey[300],
+              width: 200,
+              padding: const EdgeInsets.all(10),
+              child: const Text(
+                "Box 1",
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              ),
+            )
+          ],
+        ));
+  }
+}
